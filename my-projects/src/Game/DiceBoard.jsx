78620d68fadb76bf495aa3213ec67a161538a80b
niamul007 <am3994566@gmail.com>
2026-01-03 20:35:53 +0600

    adding all the content

--- my-projects/src/Game/DiceBoard.jsx
+++ my-projects/src/Game/DiceBoard.jsx
@@ -1,10 +1,8 @@
 import Die from "./Die";
 
-export default function DiceBoard({ holdNum, setNum }) {
-
-
+export default function DiceBoard({ holdNum,toggleHold}) {
   const diceElements = holdNum.map((die) => (
-    <Die key={die.id} num={die.num} />
+    <Die key={die.id} num={die.num} isHeld={die.isHeld} toggleHold={toggleHold} id={die.id} />
   ));
 
   return (
