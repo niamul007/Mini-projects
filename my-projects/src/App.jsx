0728705c096a2d7a1f1437b0534fc4fe225045d8
niamul007 <am3994566@gmail.com>
2026-01-08 22:29:50 +0600

    adding dashboard for logic practice

--- my-projects/src/App.jsx
+++ my-projects/src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 import PasswordValidator from './components/Locker.jsx'
 import SecretToggle from './components/Toggle.jsx'
 import Game from './Game/Game.jsx'
 import HighLow from './High-Low/highLow.jsx'
 import EmojiGame from './emoji-game/EmojiGame.jsx'
+import BudgetList from './list/List.jsx'
+import KanbanBoard from './Unknown/Known.jsx'
+import FinanceDashboard from './Dashboard/Dashboard.jsx'
 
 function App() {
 
   return (
     <>
-    <EmojiGame />
+    <FinanceDashboard/>
     </>
   )
 }
