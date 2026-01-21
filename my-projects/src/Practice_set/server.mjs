12918f0f06b1e014aa4a8cb9e1d29de752067a80
niamul007 <am3994566@gmail.com>
2026-01-21 22:20:55 +0600

    adding important info

--- my-projects/src/Practice_set/server.mjs
+++ my-projects/src/Practice_set/server.mjs
@@ -4,6 +4,11 @@ const PORT = 3000;
 const HOSTNAME = 'localhost';
 
 const server = http.createServer((req,res)=>{
+    const urlObject = new URL(req.url, `http://${req.headers.host}`);
+    const obj = Object.fromEntries(urlObject.searchParams);
+    console.log('Full URL:', urlObject.href);
+    console.log('Pathname:', urlObject.pathname);
+    console.log('Query Parameters:');
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
     res.end('Hello World\n');
