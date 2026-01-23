6c08852c924778aa3669ec3990b8292ebb533513
niamul007 <am3994566@gmail.com>
2026-01-23 21:14:36 +0600

    adding serving data

--- my-projects/src/Practice_set/server.mjs
+++ my-projects/src/Practice_set/server.mjs
@@ -1,19 +1,34 @@
-import http from 'node:http';
+import http from "node:http";
+import getAllLocations from "./byPass.js"; // Added .mjs extension
 
 const PORT = 3000;
-const HOSTNAME = 'localhost';
+const HOSTNAME = "localhost";
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(async(req, res) => {
+  try {
+
     const urlObject = new URL(req.url, `http://${req.headers.host}`);
     const obj = Object.fromEntries(urlObject.searchParams);
-    console.log('Full URL:', urlObject.href);
-    console.log('Pathname:', urlObject.pathname);
-    console.log('Query Parameters:');
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World\n');
-})
 
-server.listen(PORT,HOSTNAME,()=>{
-    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+    const destions = await getAllLocations();
+
+    if(urlObject.pathname  === '/api' && req.method === 'GET') {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(destions ,null,2));
+      return;
+    }else{
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ message: "Route not found" }));
+        return;
+    }
+
+  } catch (err) {
+    console.error("Error parsing URL:", err);
+  }
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
