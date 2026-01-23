f153831f814e1abbd10c86f078ecdb745bc2c8ec
niamul007 <am3994566@gmail.com>
2026-01-23 21:58:55 +0600

    adding cors operation

--- my-projects/src/Practice_set/utility.mjs
+++ my-projects/src/Practice_set/utility.mjs
@@ -1,5 +1,7 @@
 // This helper handles the headers AND the ending of the response
 export default function sendJSON(res, statusCode, data) {
     res.writeHead(statusCode, { "Content-Type": "application/json" });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.end(JSON.stringify(data, null, 2));
 }
